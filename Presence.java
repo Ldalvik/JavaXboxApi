6a71ba5b8d62079f981ae074a8aa7fc4ce7435ea
Julian Osborne <subaruforge@gmail.com>
2017-08-18 18:24:21 -0400

    fixed different json causing execution to stop

--- Presence.java
+++ Presence.java
@@ -1,9 +1,12 @@
 package xboxapi;
 
 import org.json.simple.JSONObject;
+import xboxapi.Root.Setup.Setup;
 
-import static xboxapi.Root.Setup.Setup.*;
-import static xboxapi.Root.Setup.Utils.*;
+import static xboxapi.Root.Setup.Setup.String;
+import static xboxapi.Root.Setup.Setup.objString;
+import static xboxapi.Root.Setup.Utils.deleteAfter;
+import static xboxapi.Root.Setup.Utils.delete_to;
 
 public class Presence {
 
@@ -11,7 +14,7 @@ public class Presence {
         return String(json, "state");
     }
     private static JSONObject LastSeen(String json){
-        return JSONObject(json, "lastSeen");
+        return Setup.JSONObject(json, "lastSeen");
     }
     public static String Device(String json, Boolean isOnline){
         String device = "User is online; Cannot display 'Device'";
@@ -23,7 +26,7 @@ public class Presence {
         return device;
     }
     public static String LastSeenPlaying(String json, Boolean isOnline){
-        String lastplayed = null;
+        String lastplayed = "User is online; Cannot display 'LastSseenPlaying'";
         if(isOnline==false) {
             lastplayed = objString(LastSeen(json), "titleName");
         } else if(isOnline==true){
@@ -43,11 +46,15 @@ public class Presence {
     public static String CurrentActivity(String json, Boolean isOnline){
         String activity = null;
         if(isOnline==true) {
+            try {
             String trimOne = delete_to(json, "},{\"id\"");
             String trimTwo = deleteAfter(trimOne, "]}]}");
             String jsonString = "{\"id\"" + trimTwo;
-            JSONObject activityObj = JSONObject(jsonString, "activity");
+            JSONObject activityObj = Setup.JSONObject(jsonString, "activity");
             activity = objString(activityObj, "richPresence");
+            }catch(StringIndexOutOfBoundsException e) {
+                activity = "StringIndexOutOfBoundsException: Cannot display 'CurrentActivity'";
+            }
         } else if(isOnline==false) {
             activity = "User is offline; Cannot display 'CurrentActivity'";
         }
@@ -56,13 +63,17 @@ public class Presence {
     public static String CurrentGame(String json, Boolean isOnline){
         String game = null;
         if(isOnline==true) {
-            String trimOne = delete_to(json, "},{\"id\"");
-            String trimTwo = deleteAfter(trimOne, "]}]}");
-            String jsonString = "{\"id\"" + trimTwo;
-            game = String(jsonString, "name");
-        } else if(isOnline==false) {
-            game = "User is offline; Cannot display 'CurrentGame'";
-        }
+            try {
+                String trimOne = delete_to(json, "},{\"id\"");
+                String trimTwo = deleteAfter(trimOne, "]}]}");
+                String jsonString = "{\"id\"" + trimTwo;
+                game = String(jsonString, "name");
+            }catch(StringIndexOutOfBoundsException e){
+                game = "StringIndexOutOfBoundsException: Cannot display 'CurrentGame'";
+            }
+            } else if (isOnline == false) {
+                game = "User is offline; Cannot display 'CurrentGame'";
+            }
         return game;
     }
     public static String All(String json, Boolean isOnline){
